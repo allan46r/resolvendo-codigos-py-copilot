f009b851679fb92af3472c2df3741954e65e0fba
allan46r <allan46r@gmail.com>
2026-03-16 20:33:50 -0300

    Update ope_mat.py

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,7 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+numero1 = int(input())
+numero2 = int(input())
+
+resultado = numero1 + numero2
+
+print(resultado)
